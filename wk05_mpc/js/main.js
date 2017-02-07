053c01ce233c5c376ad54dc97755bc9d22eb6029
Martina Calluori <martinacalluori@n3-123-231.dhcp.drexel.edu>
2017-02-07 12:28:30 -0500

    Fixed week 5

--- wk05_mpc/js/main.js
+++ wk05_mpc/js/main.js
@@ -1,2 +1,46 @@
 //
 // Declare global variables outside of functions here
+
+var ageSliderObj = document.getElementById('ageSlider');
+var displayMsgObj = document.getElementById('displayMsg');
+
+//domtraditional
+//ageSliderObj.addEventListener('change', userAgeFun, false);
+//ageSliderObj.addEventListener('input', userAgeFun, false);
+
+//doom II
+ageSliderObj.addEventListener('input', function(evt){
+	userAgeFun(evt, true);
+}, false); 
+
+function userAgeFun(evt, disAgeOnly) {
+	console.log('userAgeFun called');
+	var target = evt.target;
+	console.log('target is ' + target);
+	var age = target.value;
+	console.log('age is ' + age);
+	//
+	if (disAgeOnly == true){
+		displayMsgObj.innerHTML = age;
+		return false;
+	}
+	//
+	var msg= '';
+	if (age <= 12){
+		//child
+		msg += age + '.. child';
+	} else if (age <= 19) {
+		//teenage
+		msg += age + '.. teenager';
+	} else if (age <= 24){
+		//young adult
+		msg += age + '.. young adult';
+	} else if (age <= 64) {
+		//adult
+		msg += age + '.. adult';
+	} else {
+		//senior
+		msg += age + '.. senior';
+	} //end if
+	displayMsg.innerHTML = msg;
+} //end userAgeFun
